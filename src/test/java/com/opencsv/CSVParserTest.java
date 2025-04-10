bcourtine/opencsv

commit c20025253285ab42c1032f54c69370fff78b6ea3

small optimizations based on profiler findings - keep a string version of quote and separator to reduce the number of calls to Character.toString

src/main/java/com/opencsv/AbstractCSVParser.java
@@ -4,6 +4,7 @@ import com.opencsv.enums.CSVReaderNullFieldIndicator;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,13 +14,25 @@ import java.util.stream.Stream;
  */
 public abstract class AbstractCSVParser implements ICSVParser {
     /**
+     * This is needed by the split command in case the separator character is a regex special character.
+     */
+    protected static final Pattern SPECIAL_REGEX_CHARS = Pattern.compile("[{}()\\[\\].+*?^$\\\\|]");
+    /**
      * This is the character that the CSVParser will treat as the separator.
      */
     protected final char separator;
     /**
+     * This is the separator in Stirng form to reduce the number of calls to toString.
+     */
+    protected final String separatorAsString;
+    /**
      * This is the character that the CSVParser will treat as the quotation character.
      */
     protected final char quotechar;
+    /**
+     * This is the quotechar in String form to reduce the number of calls to toString.
+     */
+    protected final String quotecharAsString;
     /**
      * Determines the handling of null fields.
      *
@@ -41,7 +54,10 @@ public abstract class AbstractCSVParser implements ICSVParser {
      */
     public AbstractCSVParser(char separator, char quotechar, CSVReaderNullFieldIndicator nullFieldIndicator) {
         this.separator = separator;
+        this.separatorAsString = SPECIAL_REGEX_CHARS.matcher(Character.toString(separator)).replaceAll("\\\\$0");
+        ;
         this.quotechar = quotechar;
+        this.quotecharAsString = Character.toString(quotechar);
         this.nullFieldIndicator = nullFieldIndicator;
     }
 
@@ -50,9 +66,23 @@ public abstract class AbstractCSVParser implements ICSVParser {
         return separator;
     }
 
+    /**
+     * @return String version of separator to reduce number of calls to toString.
+     */
+    public String getSeparatorAsString() {
+        return separatorAsString;
+    }
+
     @Override
     public char getQuotechar() {
         return quotechar;
+    }
+
+    /**
+     * @return String version of quotechar to reduce the number of calls to toString.
+     */
+    public String getQuotecharAsString() {
+        return quotecharAsString;
     }
 
     @Override
@@ -75,7 +105,7 @@ public abstract class AbstractCSVParser implements ICSVParser {
     public String parseToLine(String[] values, boolean applyQuotesToAll) {
         return Stream.of(values)
                 .map(v -> convertToCsvValue(v, applyQuotesToAll))
-                .collect(Collectors.joining(Character.toString(getSeparator())));
+                .collect(Collectors.joining(getSeparatorAsString()));
     }
 
     /**
@@ -102,7 +132,7 @@ public abstract class AbstractCSVParser implements ICSVParser {
             return true;
         }
 
-        return forceSurround || value.contains(Character.toString(getSeparator())) || value.contains(NEWLINE);
+        return forceSurround || value.contains(getSeparatorAsString()) || value.contains(NEWLINE);
     }
 
     /**

src/main/java/com/opencsv/RFC4180Parser.java
@@ -7,7 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.regex.Pattern;
 
 /**
  * This Parser is meant to parse according to the RFC4180 specification.
@@ -36,21 +35,6 @@ import java.util.regex.Pattern;
 public class RFC4180Parser extends AbstractCSVParser {
 
     /**
-     * This is needed by the split command in case the separator character is a regex special character.
-     */
-    private static final Pattern SPECIAL_REGEX_CHARS = Pattern.compile("[{}()\\[\\].+*?^$\\\\|]");
-
-    /**
-     * Separator character as String (used for split command).
-     */
-    private final String separatorAsString;
-
-    /**
-     * Quotation character as String (used for split command).
-     */
-    private final String quoteCharString;
-
-    /**
      * Default constructor for the RFC4180Parser.  Uses values from the ICSVParser.
      */
     public RFC4180Parser() {
@@ -66,18 +50,16 @@ public class RFC4180Parser extends AbstractCSVParser {
      */
     RFC4180Parser(char quoteChar, char separator, CSVReaderNullFieldIndicator nullFieldIndicator) {
         super(separator, quoteChar, nullFieldIndicator);
-        this.separatorAsString = SPECIAL_REGEX_CHARS.matcher(Character.toString(separator)).replaceAll("\\\\$0");
-        this.quoteCharString = Character.toString(quoteChar);
     }
 
     @Override
     protected String convertToCsvValue(String value, boolean applyQuotesToAll) {
         String testValue = (value == null && !nullFieldIndicator.equals(CSVReaderNullFieldIndicator.NEITHER)) ? "" : value;
         StringBuilder builder = new StringBuilder(testValue == null ? MAX_SIZE_FOR_EMPTY_FIELD : (testValue.length() * 2));
-        boolean containsQuoteChar = testValue != null && testValue.contains(Character.toString(getQuotechar()));
+        boolean containsQuoteChar = testValue != null && testValue.contains(getQuotecharAsString());
         boolean surroundWithQuotes = applyQuotesToAll || isSurroundWithQuotes(value, containsQuoteChar);
 
-        String convertedString = !containsQuoteChar ? testValue : testValue.replaceAll(Character.toString(getQuotechar()), Character.toString(getQuotechar()) + Character.toString(getQuotechar()));
+        String convertedString = !containsQuoteChar ? testValue : testValue.replaceAll(getQuotecharAsString(), getQuotecharAsString() + getQuotecharAsString());
 
         if (surroundWithQuotes) {
             builder.append(getQuotechar());
@@ -196,7 +178,7 @@ public class RFC4180Parser extends AbstractCSVParser {
     }
 
     private boolean startsButDoesNotEndWithQuote(String lastElement) {
-        return lastElement.startsWith(Character.toString(quotechar)) && !lastElement.endsWith(Character.toString(quotechar));
+        return lastElement.startsWith(getQuotecharAsString()) && !lastElement.endsWith(getQuotecharAsString());
     }
 
     private int findEndOfFieldFromPosition(String nextLine, int currentPosition) {
@@ -224,11 +206,11 @@ public class RFC4180Parser extends AbstractCSVParser {
     private String handleQuotes(String element) {
         String ret = element;
 
-        if (!hasOnlyOneQuote(ret) && ret.startsWith(quoteCharString)) {
-            ret = StringUtils.removeStart(ret, quoteCharString);
-            ret = StringUtils.removeEnd(ret, quoteCharString);
+        if (!hasOnlyOneQuote(ret) && ret.startsWith(getQuotecharAsString())) {
+            ret = StringUtils.removeStart(ret, getQuotecharAsString());
+            ret = StringUtils.removeEnd(ret, getQuotecharAsString());
         }
-        ret = StringUtils.replace(ret, quoteCharString + quoteCharString, quoteCharString);
+        ret = StringUtils.replace(ret, getQuotecharAsString() + getQuotecharAsString(), getQuotecharAsString());
         if (ret.isEmpty() && (nullFieldIndicator == CSVReaderNullFieldIndicator.BOTH || nullFieldIndicator == CSVReaderNullFieldIndicator.EMPTY_QUOTES)) {
             ret = null;
         }

src/main/java/com/opencsv/CSVParser.java
@@ -170,7 +170,7 @@ public class CSVParser extends AbstractCSVParser {
         boolean containsSeparatorChar = StringUtils.contains(testValue, getSeparator());
         boolean surroundWithQuotes = applyQuotestoAll || isSurroundWithQuotes(value, containsSeparatorChar);
 
-        String convertedString = !containsQuoteChar ? testValue : testValue.replaceAll(Character.toString(getQuotechar()), Character.toString(getQuotechar()) + getQuotechar());
+        String convertedString = !containsQuoteChar ? testValue : testValue.replaceAll(getQuotecharAsString(), getQuotecharAsString() + getQuotecharAsString());
         convertedString = !containsEscapeChar ? convertedString : convertedString.replace(Character.toString(getEscape()), Character.toString(getEscape()) + getEscape());
 
         if (surroundWithQuotes) {

commit 5efc0d401137fb12f0530126e5616bf88c3d3992

Fix handling of regex-unsafe separators
Was broken since c20025253285ab42c1032f54c69370fff78b6ea3 (release 5.8)

src/main/java/com/opencsv/AbstractCSVParser.java
@@ -15,21 +15,11 @@ import java.util.stream.Stream;
  */
 public abstract class AbstractCSVParser implements ICSVParser {
     /**
-     * This is needed by the split command in case the separator character is a regex special character.
-     */
-    protected static final Pattern SPECIAL_REGEX_CHARS = Pattern.compile("[{}()\\[\\].+*?^$\\\\|]");
-
-    /**
-     * Empty StringBuilder
-     */
-    protected static final StringBuilder EMPTY_STRINGBUILDER = new StringBuilder("");
-
-    /**
      * This is the character that the CSVParser will treat as the separator.
      */
     protected final char separator;
     /**
-     * This is the separator in Stirng form to reduce the number of calls to toString.
+     * This is the separator in String form to reduce the number of calls to toString.
      */
     private final String separatorAsString;
     /**
@@ -83,7 +73,7 @@ public abstract class AbstractCSVParser implements ICSVParser {
      */
     public AbstractCSVParser(char separator, char quotechar, CSVReaderNullFieldIndicator nullFieldIndicator) {
         this.separator = separator;
-        this.separatorAsString = SPECIAL_REGEX_CHARS.matcher(Character.toString(separator)).replaceAll("\\\\$0");
+        this.separatorAsString = Character.toString(separator);
 
         this.quotechar = quotechar;
         this.quotecharAsString = Character.toString(quotechar);

src/main/java/com/opencsv/RFC4180Parser.java
@@ -48,7 +48,7 @@ public class RFC4180Parser extends AbstractCSVParser {
      * @param quoteChar The character to use for quoted elements
      * @param nullFieldIndicator Indicate what should be considered null
      */
-    RFC4180Parser(char quoteChar, char separator, CSVReaderNullFieldIndicator nullFieldIndicator) {
+    protected RFC4180Parser(char quoteChar, char separator, CSVReaderNullFieldIndicator nullFieldIndicator) {
         super(separator, quoteChar, nullFieldIndicator);
     }
 
@@ -114,7 +114,7 @@ public class RFC4180Parser extends AbstractCSVParser {
     }
 
     private String[] tokenizeStringIntoArray(String nextLine) {
-        return nextLine.split(getSeparatorAsString(), -1);
+        return StringUtils.splitPreserveAllTokens(nextLine, separator);
     }
 
     private String[] handleEmptySeparators(String[] strings) {
@@ -210,7 +210,7 @@ public class RFC4180Parser extends AbstractCSVParser {
             ret = StringUtils.removeStart(ret, getQuotecharAsString());
             ret = StringUtils.removeEnd(ret, getQuotecharAsString());
         }
-        ret = StringUtils.replace(ret, getQuotecharAsString() + getQuotecharAsString(), getQuotecharAsString());
+        ret = StringUtils.replace(ret, getQuoteDoubledAsString(), getQuotecharAsString());
         if (ret.isEmpty() && (nullFieldIndicator == CSVReaderNullFieldIndicator.BOTH || nullFieldIndicator == CSVReaderNullFieldIndicator.EMPTY_QUOTES)) {
             ret = null;
         }

src/test/java/com/opencsv/CSVParserTest.java
@@ -3,7 +3,6 @@ package com.opencsv;
 import com.opencsv.enums.CSVReaderNullFieldIndicator;
 import com.opencsv.exceptions.CsvMultilineLimitBrokenException;
 import com.opencsv.exceptions.CsvValidationException;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.*;
 
 import java.io.FileReader;
@@ -861,6 +860,15 @@ public class CSVParserTest {
     public void parseToLineApplyQuotesToAllIsTrue() {
         String[] items = {"This", " is", " a", " test."};
         assertEquals("\"This\",\" is\",\" a\",\" test.\"", csvParser.parseToLine(items, true));
+    }
+
+    @Test
+    public void parseToLineUsesCorrectSeparator() {
+        CSVParserBuilder builder = new CSVParserBuilder();
+        ICSVParser parser = builder.withSeparator('.').withQuoteChar('\'').build();
+
+        String[] items = {"This", " is", " a", " test."};
+        assertEquals("This. is. a.' test.'", parser.parseToLine(items, false));
     }
 
     /**